85647af988420df038e40b9084d080c409689ce0
dotMorten <mn@iter.dk>
2013-02-07 18:16:12 -0800

    Added unit tests

---
+++ src/UnitTests/WP8/HttpClientTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Win8UnitTest
+{
+    [TestClass]
+	public class HttpClientTests
+    {
+        [TestMethod]
+		public async Task GetAsyncTest1()
+        {
+			HttpClient client = new HttpClient();
+			var response = await client.GetAsync("http://www.microsoft.com");
+			Assert.IsTrue(response.IsSuccessStatusCode);
+			Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+		}
+
+		[TestMethod]
+		public async Task GetAsyncTest_Compressed()
+		{
+			HttpClient client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip });
+			var response = await client.GetAsync("http://www.microsoft.com");
+			Assert.IsTrue(response.IsSuccessStatusCode);
+			Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+		}
+		[TestMethod]
+		public async Task GetAsyncTest_POST()
+		{
+			byte[] bytes = Encoding.UTF8.GetBytes("Foo");
+			System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes);
+			StreamContent streamContent = new StreamContent(ms);
+			HttpClient client = new HttpClient();
+			var response = await client.PostAsync("http://www.microsoft.com", streamContent);
+			Assert.IsFalse(response.IsSuccessStatusCode);
+			Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.MethodNotAllowed);
+		}
+	}
+}
---
+++ src/UnitTests/WP8/HttpMethodTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Net.Http;
+
+namespace Win8UnitTest
+{
+    [TestClass]
+	public class HttpMethodTests
+    {
+        [TestMethod]
+        public void MethodsTest()
+        {
+			Assert.AreEqual(HttpMethod.Delete.Method, "DELETE");
+			Assert.AreEqual(HttpMethod.Get.Method, "GET");
+			Assert.AreEqual(HttpMethod.Head.Method, "HEAD");
+			Assert.AreEqual(HttpMethod.Options.Method, "OPTIONS");
+			Assert.AreEqual(HttpMethod.Post.Method, "POST");
+			Assert.AreEqual(HttpMethod.Put.Method, "PUT");
+			Assert.AreEqual(HttpMethod.Trace.Method, "TRACE");
+		}
+
+		[TestMethod]
+		public void EqualsTest()
+		{
+			Assert.IsTrue(HttpMethod.Get.Equals(new HttpMethod("GET")));
+		}
+
+		[TestMethod]
+		public void ToStringTest()
+		{
+			Assert.AreEqual(HttpMethod.Get.ToString(), "GET");
+		}
+	}
+}
